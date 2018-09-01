c8a9a1cb56c645400740121e1742da682fadf2d6
aaron_huangxiaoguo@163.com <woaini1314>
2018-09-02 03:56:19 +0800

    Initial commit

---
+++ app/src/main/java/cn/tsou/hxgpopupwindow/MainActivity.java
@@ -0,0 +1,62 @@
+package cn.tsou.hxgpopupwindow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
+import android.widget.Button;
+import android.widget.TextView;
+
+import cn.tsou.hxgpopup.HxgPopupUtils;
+import tsou.cn.lib_hxgioc.HxgBind;
+import tsou.cn.lib_hxgioc.HxgContentView;
+import tsou.cn.lib_hxgioc.HxgOnClick;
+import tsou.cn.lib_hxgioc.HxgViewUtils;
+
+@HxgContentView(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * HxgPopup
+     **/
+    @HxgBind(R.id.tv_address)
+    private TextView mTvAddress;
+    private AnimationSet as;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        HxgViewUtils.getView().inject(this);
+        AlphaAnimation aa = new AlphaAnimation(0, 1); // 从完全透明，到完全不透明
+        aa.setDuration(500);
+
+        // 以(0,0.5f)为中心，从50% 放大至100%
+        ScaleAnimation sa = new ScaleAnimation(1f, 1, 0.5f, 1,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0f);
+        sa.setDuration(500);
+        as = new AnimationSet(false);
+        as.addAnimation(aa);
+        as.addAnimation(sa);
+    }
+
+    @HxgOnClick(R.id.btn_address)
+    private void btnAddressClick(Button view) {
+        View popupView = HxgPopupUtils.getInstance()
+                .setContentView(this, R.layout.popup_item)
+                .setLayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, -2)
+                .setFocusable(true)
+                .dismissPopWindow(R.id.view)
+//                .showAsDropDown(mTvAddress)
+                .setBackgroundDrawable()
+                .showAtLocation(mTvAddress, Gravity.TOP, 100, 300)
+                .setAnimation(as)
+                .getView();
+        ((TextView) popupView.findViewById(R.id.textview))
+                .setText("我是更改的PopupWindow");
+    }
+}
---
+++ hxgpopup/src/main/java/cn/tsou/hxgpopup/HxgPopupUtils.java
@@ -0,0 +1,111 @@
+package cn.tsou.hxgpopup;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.widget.PopupWindow;
+
+/**
+ * Created by 黄家三少 on 2018/9/2.
+ * PopupWindow
+ */
+
+public class HxgPopupUtils {
+
+    private View view;
+    private PopupWindow popWindow;
+    private static HxgPopupUtils instance;
+
+    static {
+        instance = new HxgPopupUtils();
+    }
+
+    private HxgPopupUtils() {
+
+    }
+
+    public static HxgPopupUtils getInstance() {
+        return instance;
+    }
+
+    //添加布局
+    public HxgPopupUtils setContentView(Context context, int layoutId) {
+        dismissPopWindow();
+        view = LayoutInflater.from(context).inflate(layoutId, null);
+        return this;
+    }
+
+    //设置宽高
+    public HxgPopupUtils setLayoutParams(int width, int height) {
+        popWindow = new PopupWindow(view, width, height);
+        return this;
+    }
+
+    //  如果要执行动画，popWindow 必须得有背景
+    public HxgPopupUtils setBackgroundDrawable() {
+        // 添加一个完全透明的背景
+        popWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        return this;
+    }
+
+    //  如果点击空白处时，隐藏掉pop窗口
+    public HxgPopupUtils setFocusable(boolean isFocusable) {
+        // 添加一个完全透明的背景
+        popWindow.setFocusable(isFocusable);
+        return this;
+    }
+
+    //  设置显示位置
+    public HxgPopupUtils showAsDropDown(View view) {
+        popWindow.showAsDropDown(view);
+        return this;
+    }
+
+    //  设置显示位置
+    public HxgPopupUtils showAtLocation(View view, int gravity, int x, int y) {
+        popWindow.showAtLocation(view, gravity, x, y);
+        return this;
+    }
+
+    //  添加动画 效果
+    public HxgPopupUtils setAnimation(Animation animation) {
+        // 只有view 才有执行动画的功能,
+        view.startAnimation(animation);
+        return this;
+    }
+
+    //  添加动画 效果
+    public HxgPopupUtils setAnimation(AnimationSet animation) {
+        // 只有view 才有执行动画的功能,
+        view.startAnimation(animation);
+        return this;
+    }
+
+    //获得布局view
+    public View getView() {
+        return view;
+    }
+
+    //根据view取消PopWindow
+    public HxgPopupUtils dismissPopWindow(int viewId) {
+        view.findViewById(viewId).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismissPopWindow();
+            }
+        });
+        return this;
+    }
+
+    //取消PopWindow
+    public void dismissPopWindow() {
+        if (popWindow != null && popWindow.isShowing()) {
+            popWindow.dismiss();
+            popWindow = null;
+        }
+    }
+}
